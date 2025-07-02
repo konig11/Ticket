5e1581338d4261a1ad6c24546e2cf62627ae196c
laerson samuel <88884632+konig11@users.noreply.github.com>
2025-07-02 13:03:45 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(cors());
@@ -42,17 +43,55 @@ app.get('/api/bilhetes/:codigo', (req, res) => {
 app.use(express.json()); // para interpretar JSON no corpo da requisição
 
 app.post('/api/bilhetes', (req, res) => {
-    const { code, game, quantity, total, method, date } = req.body;
+    const { code, game, quantity, total, method, date,dateGame,buyer,email } = req.body;
 
-    const sql = 'INSERT INTO bilhete (codigo, jogo, quantidade, total, metodo_pagamento, data_compra) VALUES (?, ?, ?, ?, ?, ?)';
-    const values = [code, game, quantity, total, method, date];
+    const sql = 'INSERT INTO bilhete (codigo, jogo, quantidade, total, metodo_pagamento, data_compra,Buyer,Email,data_jogo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [code, game, quantity, total, method, date,buyer,email,dateGame];
 
-    db.query(sql, values, (err, result) => {
+    db.query(sql, values, async (err, result) => {
         if (err) {
             console.error("Erro ao inserir bilhete:", err);
             return res.status(500).json({ error: 'Erro ao salvar bilhete no banco.' });
         }
-        res.json({ success: true, id: result.insertId });
+
+        // Send email
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', // or use SMTP for production
+        auth: {
+            user: 'laersonrui@gmail.com',
+            pass: 132214
+        },
+    });
+
+    const mailOptions = {
+        from: 'laersonrui@gmail.com',
+        to: email,
+        subject: 'Seu bilhete foi confirmado 🎟️',
+        text: `
+Olá ${buyer},
+
+Seu bilhete para o jogo ${game} foi confirmado com sucesso!
+
+📅 Data do Jogo: ${dateGame}
+🔢 Código do Bilhete: ${code}
+🎟️ Quantidade: ${quantity}
+💳 Método de Pagamento: ${method}
+💰 Total: ${total}
+
+Obrigado por sua compra!
+
+- Equipe de Gestão de Bilhetes
+        `,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+       res.json({ success: true, id: result.insertId });
+    } catch (error) {
+        console.error('Erro ao enviar email:', error);
+        return res.json({ success: false, message: 'Falha ao enviar email.' });
+    }
+        
     });
 });
 app.listen(3001, () => {
